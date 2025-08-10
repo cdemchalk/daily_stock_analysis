0362b6d02fb81c682b64b63f402e7d293bc47728
cdemchalk <30451286+cdemchalk@users.noreply.github.com>
2025-08-10 14:50:46 -0500

    corrections to github dailyrunner files

--- DailyRunner/main_function.py
+++ DailyRunner/main_function.py
@@ -8,8 +8,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 if BASE_DIR not in sys.path:
     sys.path.append(BASE_DIR)
 
-import main  # your existing main.py
+import main1  # your existing main.py
 
 def main_function(timer) -> None:
     logging.info("Azure Function triggered.")
-    main.run()
+    main1.run()
